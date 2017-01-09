1e48dd996724f950a96c439153ce39e40f9c1fe7
Javier Luraschi <javierluraschi@hotmail.com>
2017-01-09 15:50:33 -0800

    use temp inmemory cache for warc

--- java/SparkWARC.scala
+++ java/SparkWARC.scala
@@ -7,7 +7,8 @@ import org.apache.spark.sql._
 object WARC {
   def load(sc: SparkContext, path: String) : DataFrame = {
     val warc = sc.textFile(path)
-    val warcIdx = warc.zipWithIndex.map(_.swap)
+    val warcIdx = warc.zipWithIndex.map(_.swap).cache()
+
     val warcMarksFirst = warcIdx.filter(t => t._2 == "WARC/1.0").map(t => t._1)
     val warcMarksSecond = warcMarksFirst.zipWithIndex.filter(t => t._2 != 0).map(t => t._1)
 
@@ -25,6 +26,9 @@ object WARC {
     val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
 
-    warcPaged.toDF
+    val df = warcPaged.toDF
+    warcIdx.unpersist()
+
+    df
   }
 }
