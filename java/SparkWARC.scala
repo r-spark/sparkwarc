7afd098fddf20de1ccd748bacbbbca41b5149cd8
Javier Luraschi <javierluraschi@hotmail.com>
2017-01-09 15:27:11 -0800

    use textfile and map to avoid 2gb rdd entry limit

--- java/SparkWARC.scala
+++ java/SparkWARC.scala
@@ -6,12 +6,25 @@ import org.apache.spark.sql._
 
 object WARC {
   def load(sc: SparkContext, path: String) : DataFrame = {
-    val warc = sc.wholeTextFiles(path)
-    val warcpg = warc.flatMap(t => t._2.split("WARC/1.0"))
+    val warc = sc.textFile(path)
+    val warcIdx = warc.zipWithIndex.map(_.swap)
+    val warcMarksFirst = warcIdx.filter(t => t._2 == "WARC/1.0").map(t => t._1)
+    val warcMarksSecond = warcMarksFirst.zipWithIndex.filter(t => t._2 != 0).map(t => t._1)
+
+    val warcMarksFirstIdx = warcMarksFirst.zipWithIndex.map(_.swap)
+    val warcMarksSecondIdx = warcMarksSecond.zipWithIndex.map(_.swap)
+
+    val warcJoined = warcMarksFirstIdx.join(warcMarksSecondIdx)
+
+    val warcEntriesMap = warcJoined.flatMap(t => (t._2._1 to t._2._2).map(i => (i, t._1)))
+
+    val warcGrouped = warcIdx.join(warcEntriesMap).map(t => (t._2._2, (t._1, t._2._1))).groupByKey
+
+    val warcPaged = warcGrouped.mapValues(t => t.toList.sortBy(_._1).map(i => i._2).mkString("|")).map(i => i._2)
 
     val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
 
-    warcpg.toDF
+    warcPaged.toDF
   }
 }
