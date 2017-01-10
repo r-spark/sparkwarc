f303f38d75d2e7d2b4eb65eb783ccec0750e19b6
Javier Luraschi <javierluraschi@hotmail.com>
2017-01-09 16:19:31 -0800

    additional caching steps to improve perf

--- R/sparkwarc.R
+++ R/sparkwarc.R
@@ -4,6 +4,7 @@
 #'
 #' @param sc An active \code{spark_connection}.
 #' @param path The path to the warc file.
+#' @param name The name of the temp table.
 #' @param ... Additional arguments reserved for future use.
 #'
 #' @examples
@@ -18,11 +19,12 @@
 #' spark_disconnect(sc)
 #'
 #' @export
-spark_read_warc <- function(sc, path, ...) {
+spark_read_warc <- function(sc, path, name, ...) {
   sparklyr::invoke_static(
     sc,
     "SparkWARC.WARC",
     "load",
     spark_context(sc),
-    path)
+    path,
+    name)
 }
--- java/SparkWARC.scala
+++ java/SparkWARC.scala
@@ -5,9 +5,11 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 
 object WARC {
-  def load(sc: SparkContext, path: String) : DataFrame = {
+  def load(sc: SparkContext, path: String, name: String) : DataFrame = {
     val warc = sc.textFile(path)
     val warcIdx = warc.zipWithIndex.map(_.swap).cache()
+
+    warcIdx.count()
 
     val warcMarksFirst = warcIdx.filter(t => t._2 == "WARC/1.0").map(t => t._1)
     val warcMarksSecond = warcMarksFirst.zipWithIndex.filter(t => t._2 != 0).map(t => t._1)
@@ -17,7 +19,9 @@ object WARC {
 
     val warcJoined = warcMarksFirstIdx.join(warcMarksSecondIdx)
 
-    val warcEntriesMap = warcJoined.flatMap(t => (t._2._1 to t._2._2).map(i => (i, t._1)))
+    val warcEntriesMap = warcJoined.flatMap(t => (t._2._1 to t._2._2).map(i => (i, t._1))).cache
+
+    warcEntriesMap.count()
 
     val warcGrouped = warcIdx.join(warcEntriesMap).map(t => (t._2._2, (t._1, t._2._1))).groupByKey
 
@@ -26,7 +30,14 @@ object WARC {
     val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
 
-    val df = warcPaged.toDF
+    val df = warcPaged.toDF.cache()
+
+    df.registerTempTable(name)
+    df.count()
+
+    warcEntriesMap.unpersist(true)
+    warcIdx.unpersist(true)
+
     df
   }
 }
