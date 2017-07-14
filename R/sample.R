b2ea51b6062ae5580c55d167cfdcc7eb6eac901b
Javier Luraschi <javierluraschi@hotmail.com>
2017-07-14 16:59:04 -0700

    refactor sample.r

--- R/sample.R
+++ R/sample.R
@@ -1,11 +1,31 @@
-#' Loads the sample warc file
+#' Retrieves sample warc path
+#'
+#' @export
+rcpp_read_warc_sample_path <- function() {
+  normalizePath(system.file("samples/sample.warc.gz", package = "sparkwarc"))
+}
+
+#' Loads the sample warc file in Rcpp
 #'
 #' @param filter A regular expression that is applied to each warc entry
 #'   efficiently by running native code using \code{Rcpp}.
 #'
 #' @export
-spark_read_warc_sample <- function(filter = "") {
-  sample_warc <- normalizePath(system.file("samples/sample.warc.gz", package = "sparkwarc"))
+rcpp_read_warc_sample <- function(filter = "") {
+  sample_warc <- rcpp_read_warc_sample_path()
 
   sparkwarc:::rcpp_read_warc(sample_warc, filter)
+}
+
+#' Loads the sample warc file in Spark
+#'
+#' @param An active \code{spark_connection}.
+#' @param filter A regular expression that is applied to each warc entry
+#'   efficiently by running native code using \code{Rcpp}.
+#'
+#' @export
+spark_read_warc_sample <- function(sc, filter = "") {
+  sample_warc <- rcpp_read_warc_sample_path()
+
+  spark_read_warc(sc, "sample_warc", sample_warc, overwrite = TRUE, group = TRUE, filter = filter)
 }
