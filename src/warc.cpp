9443acc0ecd8cedd99d1a8344ff11dd0c1c870c4
Javier Luraschi <javierluraschi@hotmail.com>
2017-07-14 14:45:02 -0700

    add support to filter by regexs

--- src/warc.cpp
+++ src/warc.cpp
@@ -5,6 +5,7 @@ using namespace Rcpp;
 
 #include <stdio.h>
 #include <zlib.h>
+#include <regex>
 
 // [[Rcpp::export]]
 List rcpp_hello_world() {
@@ -19,6 +20,7 @@ List rcpp_hello_world() {
 // [[Rcpp::export]]
 CharacterVector rcpp_read_warc(std::string path,
                                std::string filter) {
+  std::regex filter_regex(filter);
 
   FILE *fp = fopen(path.c_str(), "rb");
   if (!fp) Rcpp::stop("Failed to open WARC file.");
@@ -35,14 +37,26 @@ CharacterVector rcpp_read_warc(std::string path,
   std::string warc_entry;
   warc_entry.reserve(warc_mean_size);
 
+  bool one_matched = false;
   const std::string warc_separator = "WARC/1.0";
   while(gzgets(gzf, buffer, buffer_size) != Z_NULL) {
-    if (std::string(buffer).substr(0, warc_separator.size()) == warc_separator && warc_entry.size() > 0) {
-      warc_entries.push_back(warc_entry);
+    std::string line(buffer);
+
+    if (filter.size() > 0 && !one_matched) {
+      std::string no_newline = line.substr(0, line.find_first_of('\n'));
+      one_matched = regex_match(no_newline, filter_regex);
+    }
+
+    if (std::string(line).substr(0, warc_separator.size()) == warc_separator && warc_entry.size() > 0) {
+      if (filter.size() == 0 || one_matched) {
+        warc_entries.push_back(warc_entry);
+      }
+
+      one_matched = false;
       warc_entry.clear();
     }
 
-    warc_entry.append(buffer);
+    warc_entry.append(line);
   }
 
   if (gzf) gzclose(gzf);
