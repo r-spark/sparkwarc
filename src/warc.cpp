402437753080e9af41b82c0cfc5e6c6c09c44c70
Javier Luraschi <javierluraschi@hotmail.com>
2017-07-14 12:46:19 -0700

    add support to open/close gz warc file

--- src/warc.cpp
+++ src/warc.cpp
@@ -2,6 +2,9 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+#include <stdio.h>
+#include <zlib.h>
+
 // [[Rcpp::export]]
 List rcpp_hello_world() {
 
@@ -10,4 +13,22 @@ List rcpp_hello_world() {
   List z            = List::create( x, y ) ;
 
   return z ;
+}
+
+// [[Rcpp::export]]
+CharacterVector rcpp_read_warc(std::string path,
+                               std::string filter) {
+
+  CharacterVector result = CharacterVector::create("");
+
+  FILE *fp = fopen(path.c_str(), "rb");
+  if (!fp) Rcpp::stop("Failed to open WARC file.");
+
+  gzFile gzf = gzdopen(fileno(fp), "rb");
+  if (!gzf) Rcpp::stop("Failed to open WARC as a compressed file.");
+
+  if (gzf) gzclose(fp);
+  if (fp) fclose(fp);
+
+  return result;
 }
